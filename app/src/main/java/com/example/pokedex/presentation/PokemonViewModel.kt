403971024716d0ef1108081ecd2299c3bc76e967
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-07-25 14:06:35 -0500

    Error en el view model y livedata

--- app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.pokedex.presentation
 
-class PokemonViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.pokedex.core.Resource
+import kotlinx.coroutines.Dispatchers
+
+class PokemonViewModel:ViewModel() {
+
+    fun fetchPokemon() = liveData(Dispatchers.IO){
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(repo.getPokemon()))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+
+    }
 }
