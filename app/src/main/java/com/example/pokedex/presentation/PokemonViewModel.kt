409caba2cef496ebd3b2bf699bbf9e2e0598a22d
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-07-25 17:46:46 -0500

    Capa de presentacion lista

--- app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.pokedex.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
 import com.example.pokedex.core.Resource
+import com.example.pokedex.repository.PokemonRepository
 import kotlinx.coroutines.Dispatchers
 
-class PokemonViewModel:ViewModel() {
+class PokemonViewModel(private val repo:PokemonRepository):ViewModel() {
 
     fun fetchPokemon() = liveData(Dispatchers.IO){
         emit(Resource.Loading())
@@ -15,5 +17,11 @@ class PokemonViewModel:ViewModel() {
             emit(Resource.Failure(e))
         }
 
+    }
+}
+
+class PokemonViewModelFactory(private val repo: PokemonRepository): ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(PokemonRepository::class.java).newInstance(repo)
     }
 }
