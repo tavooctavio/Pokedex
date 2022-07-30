ecab4957457f14244f8ae84eb4fb71448044fb0c
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-07-29 19:44:01 -0500

    error con el adapter no me aparece el card

--- app/src/main/java/com/example/pokedex/ui/adapter/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/ui/adapter/PokemonAdapter.kt
@@ -2,26 +2,47 @@ package com.example.pokedex.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.core.BaseViewHolder
 import com.example.pokedex.data.model.Pokemon
 import com.example.pokedex.data.model.PokemonList
 import com.example.pokedex.databinding.PokemonItemBinding
 
-class PokemonAdapter:RecyclerView.Adapter<BaseViewHolder<*>>() {
+class PokemonAdapter(
+    private val pokemonList: List<Pokemon>,
+    private val itemClickListener: OnPokemonClickListener
+) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    interface OnPokemonClickListener {
+        fun onPokemonClick(pokemon: Pokemon)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        val itemBiding = PokemonItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val itemBiding =
+            PokemonItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = PokemonViewHolder(itemBiding)
+
+        itemBiding.root.setOnClickListener {
+            val position =
+                holder.bindingAdapterPosition.takeIf { it != DiffUtil.DiffResult.NO_POSITION }
+                    ?: return@setOnClickListener
+            itemClickListener.onPokemonClick(pokemonList[position])
+        }
+        return holder
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        TODO("Not yet implemented")
+        when(holder){
+            is PokemonViewHolder -> holder.bind(pokemonList[position])
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = pokemonList.size
 
-    private inner class PokemonViewHolder(val binding: PokemonItemBinding): BaseViewHolder<Pokemon>(binding.root){
+    private inner class PokemonViewHolder(val binding: PokemonItemBinding) :
+        BaseViewHolder<Pokemon>(binding.root) {
         override fun bind(item: Pokemon) {
             binding.btnPokemonName.text = item.name
         }
